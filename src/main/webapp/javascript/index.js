d75cdfb739ae94f668c09965dcab7739c09e0946
Jayati <jayati984@gmail.com>
2020-04-09 15:38:11 -0700

    Add homepage with tabs to other pages (if logged in), styling, and comments feature, add HomeDataServlet.java to store comments.

--- src/main/webapp/javascript/index.js
+++ src/main/webapp/javascript/index.js
@@ -1,1 +1,20 @@
+function loadComments() {
+  fetch('/data').then(response => response.json()).then((comments) => {
+    const commentListElement = document.getElementById('comment-list');
+    comments.forEach((comment) => {
+      commentListElement.appendChild(createCommentElement(comment));
+    })
+  });
+}
 
+/** Creates an element that represents a task. */
+function createCommentElement(comment) {
+  const commentElement = document.createElement('li');
+  commentElement.className = 'comment';
+
+  const textElement = document.createElement('span');
+  textElement.innerText = comment;
+
+  commentElement.appendChild(textElement);
+  return commentElement;
+}
