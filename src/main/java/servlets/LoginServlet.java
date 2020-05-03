f4d0ccd9c6284495a2595e61bd3ae122b629e5ab
Chanel <chanelnb1@gmail.com>
2020-05-03 14:22:36 -0400

    added data query

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -2,6 +2,11 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +20,17 @@ public class LoginServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("user");
+    PreparedQuery results = datastore.prepare(query);
+        for (Entity entity : results.asIterable()) {
+            String name = (String)entity.getProperty("name");
+            String email = (String)entity.getProperty("email");
+            String password = (String)entity.getProperty("password");
+            String university = (String)entity.getProperty("university");
+            String telephone = (String)entity.getProperty("telephone");
+        }
+        
     UserService userService = UserServiceFactory.getUserService();
     if (userService.isUserLoggedIn()) {
       response.sendRedirect("/home.html");
@@ -23,5 +39,5 @@ public class LoginServlet extends HttpServlet {
       String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
       response.sendRedirect("/login.html");
     }
-  }
+}
 }
